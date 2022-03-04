ce97547151a9134527eafd6244a8cdef7ef9c37b
takashi kasai <takassh23@gmail.com>
2022-03-04 18:24:02 +0900

    update

--- scripts/v2swap.ts
+++ scripts/v2swap.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import { ethers } from "hardhat";
+import IERC20Artifact from "../artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json";
 import V2SwapArtifact from "../artifacts/contracts/V2Swap.sol/V2Swap.json";
+import { IERC20 } from "../typechain";
 import { V2Swap } from "../typechain/V2Swap";
 dotenv.config();
 
@@ -8,9 +10,11 @@ async function v2Swap() {
   console.log("start");
 
   // Connect to the network
-  const provider = ethers.getDefaultProvider("ropsten");
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.ROPSTEN_SERVER_URL ?? ""
+  );
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
-  const swapContractAddress = "";
+  const swapContractAddress = "0xB8f012e650488967f0fFa088642e5CB3667c77d8";
 
   const contract = new ethers.Contract(
     swapContractAddress,
@@ -18,15 +22,46 @@ async function v2Swap() {
     wallet
   ) as V2Swap;
 
-  console.log(contract.functions);
+  const weth = new ethers.Contract(
+    "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+    IERC20Artifact.abi,
+    wallet
+  ) as IERC20;
+
+  const dai = new ethers.Contract(
+    "0xaD6D458402F60fD3Bd25163575031ACDce07538D",
+    IERC20Artifact.abi,
+    wallet
+  ) as IERC20;
+
+  const myAddress = await wallet.getAddress();
+
+  let balance = await weth.balanceOf(myAddress);
+
+  console.log("weth balance before:", balance);
+
+  const amount = ethers.utils.parseUnits("0.1", 18);
+
+  // must approve contract to use your weth
+  const approve = await weth.approve(contract.address, amount);
+
+  await approve.wait();
 
   const result = await contract.swapExactInputSingle(
-    100000000000000000,
-    "0xc778417E063141139Fce010982780140Aa0cD5Ab",
-    "0xaD6D458402F60fD3Bd25163575031ACDce07538D"
+    amount,
+    weth.address,
+    dai.address
   );
 
-  console.log(result.hash);
+  await result.wait();
+
+  balance = await weth.balanceOf(myAddress);
+
+  console.log("weth balance after:", balance);
+
+  console.log("contract is here:", result.hash);
+
+  // weth.on("Approval", async (owner, spender, value: BigNumber) => {});
 }
 
 // We recommend this pattern to be able to use async/await everywhere
